5d35c1f21a488287831965479a1956d4081ed9aa
Robin Slager <robin.slager@northchain.tech>
2020-11-10 22:07:11 +0100

    version 0.9.1 closes #2 removed all code that is not needed

--- src/main/java/BotPackeges/DiscordController/Filewriter_Controller.java
+++ src/main/java/BotPackeges/DiscordController/Filewriter_Controller.java
@@ -1,8 +1,5 @@
 package BotPackeges.DiscordController;
 
-
-import BotPackeges.Settings.Singleton;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.*;
@@ -49,40 +46,4 @@ public class Filewriter_Controller {
             return null;
         }
     }
-
-    public JSONObject getServerInfo(String guildID) {
-        JSONObject File = new JSONObject(read());
-        JSONArray servers = File.getJSONArray("servers");
-        for (int i = 0; i < servers.length(); i++) {
-            if (Singleton.getInstance().base.get_ServerID().equals(servers.getJSONObject(i).getString("server_ID"))) {
-                return servers.getJSONObject(i);
-            }
-        }
-        return null;
-    }
-
-    public JSONObject getGitinfo(String guildID, String Discord_name){
-        JSONObject serverinfo = getServerInfo(guildID);
-        JSONArray github = serverinfo.getJSONArray("server_github");
-        for (int i = 0; i < github.length(); i++) {
-            if(github.getJSONObject(i).get("discord").equals(Discord_name)){
-                return github.getJSONObject(i);
-            }
-        }
-        return null;
-    }
-
-    public JSONArray getServerSettings(String guildID){
-        JSONObject serverinfo = getServerInfo(guildID);
-        JSONArray settings = serverinfo.getJSONArray("server_Settings");
-        return settings;
-    }
-    public JSONObject getSettingsInfo(String guildID){
-        JSONObject serverinfo = getServerInfo(guildID);
-        JSONObject settings_info = serverinfo.getJSONObject("Settings_Info");
-        return settings_info;
-    }
-
-
-
 }
